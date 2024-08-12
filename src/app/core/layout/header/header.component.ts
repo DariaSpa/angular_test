41f305d4d0f5e71aa5740b9d1a6f89872a70db32
DariaSpa <daryaskorb@gmail.com>
2024-08-12 12:43:17 +0200

    added trackBy function for menu items

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { APP_TITLE, MENU_ITEMS } from '../../../shared/constants';
+import { MenuItem } from '../../../shared/models';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,8 @@ import { APP_TITLE, MENU_ITEMS } from '../../../shared/constants';
 export class HeaderComponent {
   appTitle = APP_TITLE;
   menuItems = MENU_ITEMS;
+
+  trackByMenuItem(index: number, menuItem: MenuItem): string {
+    return menuItem.label;
+  }
 }
